a7ef315a4c75f834ce15b95eb8dd173adf0e87b9
Zugyuk <Overlordmw@hotmail.com>
2019-07-30 21:58:59 -0700

    Folders and the start of the player controller.

---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : MonoBehaviour
+{
+    public float maxSpeed = 7;
+    public float jumpImpulseSpeed = 7;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
