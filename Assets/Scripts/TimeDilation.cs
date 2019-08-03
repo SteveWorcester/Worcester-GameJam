9df62f001d57dc2e8d790a5a66025c435311ef2e
Steve Worcester <Worcester.Steve@Gmail.com>
2019-08-02 18:39:50 -0700

    - Add prototype time dilation class so ability wheel can utilize it. - Untested; requires action/ability caster to test

---
+++ Assets/Scripts/TimeDilation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TimeDilation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DilateTimeForAbility();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // TODO: This needs to have an ability arg incoming, and used in while to stop dilation after ability cast 
+    void DilateTimeForAbility()
+    {
+        float timeScaleAmount = .1f;
+        double dilationTimerInRealtimeSeconds = 1;
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        while (sw.Elapsed <= TimeSpan.FromSeconds(dilationTimerInRealtimeSeconds))
+        {
+            Time.timeScale = timeScaleAmount;
+        }
+
+    }
+}
