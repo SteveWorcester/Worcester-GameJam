7963a6ac7cf0e9cd4172c046233d78cf32e240a4
Zugyuk <Overlordmw@hotmail.com>
2019-08-21 22:10:03 -0700

    Not sure why these didn't submit
    trying again?

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//generic physics object - everything that can fall or be colided with belongs in this class, or in a child of this class.  This may, or may not include things like projectiles, monsters, etc.
+
+public class PhysicsObject : MonoBehaviour
+{
+    //speeds and gravity mods.  should be public facing
+    public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+
+    //variables influenced by the previous two through gameplay.  These shouldn't be changed manually.
+    protected Vector2 targetVelocity;
+    protected bool isGrounded;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    // we want the rigidbody to turn on immediately when a physics object is enabled - this should stop pass-through
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    void Update()  //The ONLY thing that should be in here is velocity calculations
+    {
+        targetVelocity = Vector2.zero;
+        ComputeVelocity();
+        //make the slider on child PlayerPlatformerController roatate based on current direction pressed?
+    }
+
+    protected virtual void ComputeVelocity() {}
+
+    void FixedUpdate()  //The ONLY thing allowed in here is physics stuff - collisions, checks, etc.
+    {
+        //gravity calculations come first.
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+
+        //then check if the object is touching something along the gravity axis.
+        isGrounded = false;
+
+        //time.deltatime calculation for expected position counting for velocity - and keep on the ground if it's already touching.
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        //calling movement while on the ground
+        Movement(move, false);
+
+        //This is waht we will need to change if we want to alter gravity.  later, much later.
+        move = Vector2.up * deltaPosition.y;
+
+        //calling movement while in the air
+        Movement(move, true);
+    }
+
+    void Movement(Vector2 move, bool yMovement)  //fuck my rewrites, just sticking this into a function
+    {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)  //don't let the player move if they're trying to move less than a pixel... because fuck pixel perfect jumps.
+        {
+            //count how many things the object is colliding with, so we can have multiple collisions based on inputs.
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++) {hitBufferList.Add(hitBuffer[i]);}
+
+            //use the buffer to kick back movement vectors.
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY) //fuck pixel perfect jumps
+                {
+                    isGrounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+
+        }
+
+        //Finally, calculate distance between it's position and it's normalized move value.
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+
+}
--- Assets/Scripts/PlayerPlatformerController.cs
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -1,21 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerPlatformerController : MonoBehaviour
+//this class will be built from physicsobject, and contains the sprite - and speed variables.  It also calculates its own velocity.
+
+public class PlayerPlatformerController : PhysicsObject
 {
+    //max player move speed and jump speed.
     public float maxSpeed = 7;
-    public float jumpImpulseSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    private float sliderFloat;
+    public float chargeSpeed;
+    protected Slider slider;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
 
-
-    void Start()
+    void Awake()
     {
-        
+        //grab the spriterenderer, and apply the animator, even if they won't do shit for a bit.
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        slider = GetComponent<Slider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //a velocity grabber of some sort - lets us calculate if player is going too fast, and stop it.
+    protected override void ComputeVelocity()
     {
-        
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+
+        // ChargeAim()  //REAL JUMP
+
+        if (Input.GetButtonDown("Jump") && isGrounded)  //TEMP JUMP, this is just to make me happy for a short period of time.
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        //since we're using sprites, if you're moving backwards - horizontal flip the sprite.
+        bool flipSprite = (spriteRenderer.flipX ? (move.x >= 0.01f) : (move.x <= 0.01f));
+
+        if (flipSprite)  //if moving -x, flip that sprite
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        //jumping animation bullshit
+        animator.SetBool("grounded", isGrounded);  //this will be more important if we can bother with jump animations
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    public float ChargeAim()
+    {
+        //let it charge up when jump is pressed
+        while (Input.GetButtonDown("Jump"))
+        {
+            sliderFloat = sliderFloat + chargeSpeed;
+        }
+
+        //make something happen when jump is released, based on the charge value
+
+        return sliderFloat;
     }
 }
