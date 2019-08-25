8eac046cfeff16d9d1f351b153eb76b4edc58007
Steve Worcester <Worcester.Steve@Gmail.com>
2019-08-25 11:21:11 -0700

    jump and knockback
    changed jump to charge space (maybe change to no charge?)
    added knockback on charge click

--- Assets/Scripts/TimeDilation.cs
+++ Assets/Scripts/TimeDilation.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TimeDilation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeScaleAmount = .1f;
+    public float dilationTimerInRealtimeSeconds = 1f;
+
+    /// <summary>
+    /// Don't use yet...
+    /// </summary>
+    public void StartTimeDilation()
     {
-        DilateTimeForAbility();
+        Time.timeScale = timeScaleAmount;
+        Time.fixedDeltaTime = timeScaleAmount * .02f; //.02f because this brings frame update to normal time.
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Don't use yet...
+    /// </summary>
+    public void StopTimeDilation()
     {
-        
-    }
-
-    // TODO: This needs to have an action/abilityWheel arg incoming, and used in while to stop dilation after ability cast 
-    void DilateTimeForAbility()
-    {
-        float timeScaleAmount = .1f;
-        double dilationTimerInRealtimeSeconds = 1;
-
-        Stopwatch sw = new Stopwatch();
-        sw.Start();
-        while (sw.Elapsed <= TimeSpan.FromSeconds(dilationTimerInRealtimeSeconds)) // Don't forget to put incoming action here to stop time dilation when ability is cast.
-        {
-            Time.timeScale = timeScaleAmount;
-        }
-
+        Time.timeScale += 1f;
+        Mathf.Clamp(Time.timeScale, timeScaleAmount, 1f);
     }
 }
